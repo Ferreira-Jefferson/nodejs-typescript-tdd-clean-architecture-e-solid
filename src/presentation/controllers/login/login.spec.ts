7638167ed108e680399f4f5f8914288bdcc8d5d4
Jefferson Ferreira <jefferson.ctc.ferreira@outlook.com>
2020-08-03 13:59:53 -0300

    refactor: move HttpHelper to a folder

--- src/presentation/controllers/login/login.spec.ts
+++ src/presentation/controllers/login/login.spec.ts
@@ -1,7 +1,7 @@
 import { LoginController } from './login'
 import { Authentication, HttpRequest, Validation } from './login-protocols'
 import { MissingParamError } from './../../errors'
-import { badRequest, serverError, unauthorized, ok } from './../../helpers/http-helper'
+import { badRequest, serverError, unauthorized, ok } from './../../helpers/http/http-helper'
 
 const makeFakeRequest = (): HttpRequest => ({
   body: {
