807025a6a6383d7b2f7a86a70fa163fec23c28ac
Jefferson Ferreira <jefferson.ctc.ferreira@outlook.com>
2020-08-05 17:52:34 -0300

    feat: add LoginFactory

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,4 +1,5 @@
 export default {
   mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/clean-node-api',
-  port: process.env.PORT || 5050
+  port: process.env.PORT || 5050,
+  jwt_secret: process.env.JWT_SECRET || 'a-23s4#d567z34=fsdg*e'
 }
