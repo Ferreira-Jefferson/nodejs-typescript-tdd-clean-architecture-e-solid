a902bd44277849ce8cadc96c4730c155df9f5da0
Jefferson Ferreira <jefferson.ctc.ferreira@outlook.com>
2020-08-05 18:19:21 -0300

    test: add login route

--- src/main/routes/login-route.test.ts
+++ src/main/routes/login-route.test.ts
@@ -1,8 +1,12 @@
 import request from 'supertest'
 import app from '../config/app'
 import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
+import { Collection } from 'mongodb'
+import { BcryptAdapter } from './../../infra/criptography/bcrypt-adapter/bcrypt-adapter'
 
 describe('Login Routes', () => {
+  let accountCollection: Collection
+
   beforeAll(async () => {
     await MongoHelper.connect(process.env.MONGO_URL)
   })
@@ -12,7 +16,7 @@ describe('Login Routes', () => {
   })
 
   beforeEach(async () => {
-    const accountCollection = await MongoHelper.getCollection('accounts')
+    accountCollection = await MongoHelper.getCollection('accounts')
     await accountCollection.deleteMany({})
   })
 
@@ -25,6 +29,25 @@ describe('Login Routes', () => {
           email: 'valid_email@mail.com',
           password: 'valid_password',
           passwordConfirmation: 'valid_password'
+        })
+        .expect(200)
+    })
+  })
+
+  describe('POST /login Routes', () => {
+    test('Should return 200 on login', async () => {
+      const bcryptAdapter = new BcryptAdapter(12)
+      const password = await bcryptAdapter.hash('valid_password')
+      await accountCollection.insertOne({
+        name: 'valid_name',
+        email: 'valid_email@mail.com',
+        password
+      })
+      await request(app)
+        .post('/api/login')
+        .send({
+          email: 'valid_email@mail.com',
+          password: 'valid_password'
         })
         .expect(200)
     })
