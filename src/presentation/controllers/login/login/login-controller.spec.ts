d3073e9f913bd1edd1dc244155a92edacf2f39c2
Jefferson Ferreira <jefferson.ctc.ferreira@outlook.com>
2020-08-17 13:24:44 -0300

    refactor: change models to use type alias instead of interfaces

--- src/presentation/controllers/login/login/login-controller.spec.ts
+++ src/presentation/controllers/login/login/login-controller.spec.ts
@@ -29,7 +29,7 @@ const makeAuthenticationStub = (): Authentication => {
   return new AuthenticationStub()
 }
 
-interface SutTypes {
+type SutTypes = {
   sut: LoginController
   authenticationStub: Authentication
   validationStub: Validation
--- src/validation/validators/validation-composite.spec.ts
+++ src/validation/validators/validation-composite.spec.ts
@@ -11,7 +11,7 @@ const makeValidation = (): Validation => {
   return new ValidationStubs()
 }
 
-interface SutTypes {
+type SutTypes = {
   sut: ValidationComposite
   validationStubs: Validation[]
 }
